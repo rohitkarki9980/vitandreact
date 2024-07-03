ab57217523be6a46396133e4ebda6dc8088da836
rohitkarki <rohitkarki1243@gmail.com>
2024-07-03 20:49:46 +0530

    viteandreactpractice

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from './Test'
+function App() {
+ 
+  return (
+    <>
+    <h1>hello</h1>
+    <Test/>
+    </>
+  )
+}
+
+export default App
---
+++ vitereact/src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <>
+    <h1>Test is ready</h1>
+      
+    </>
+  );
+}
+
+export default Test;
